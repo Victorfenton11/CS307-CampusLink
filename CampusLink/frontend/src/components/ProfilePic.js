1aad619010d592183623a54b4ec44fa4539bd797
maoyifei2013605 <mao154@purdue.edu>
2023-10-01 22:34:50 -0400

    Profile Picture Button

---
+++ CampusLink/frontend/src/components/ProfilePic.js
@@ -0,0 +1,99 @@
+import React from 'react'
+
+
+const ImgUpload =({
+    onChange,
+    src
+})=>
+    <label htmlFor="photo-upload" className="custom-file-upload fas">
+    <div className="img-wrap img-upload" >
+        <img htmlFor="photo-upload" src={src}/>
+    </div>
+    <input id="photo-upload" type="file" onChange={onChange}/> 
+    </label>
+
+
+
+const Profile =({
+    onSubmit,
+    src,
+})=>
+    <div className="card">
+    <form onSubmit={onSubmit}>
+        <h1>Profile Card</h1>
+        <label className="custom-file-upload fas">
+        <div className="img-wrap" >
+            <img for="photo-upload" src={src}/>
+        </div>
+        </label>
+        <button type="submit" className="edit">Edit Profile </button>
+    </form>
+    </div>
+    
+        
+const Edit =({
+    onSubmit,
+    children,
+})=>
+    <div className="card">
+    <form onSubmit={onSubmit}>
+        <h1>Profile Card</h1>
+        {children}
+        <button type="submit" className="save">Save </button>
+    </form>
+    </div>
+
+class ProfilePic extends React.Component {
+    state = {
+    file: '',
+    imagePreviewUrl: 'https://github.com/OlgaKoplik/CodePen/blob/master/profile.jpg?raw=true',
+    name:'',
+    status:'',
+    active: 'edit'
+    }
+
+    photoUpload = e =>{
+    e.preventDefault();
+    const reader = new FileReader();
+    const file = e.target.files[0];
+    reader.onloadend = () => {
+        this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+        });
+    }
+    reader.readAsDataURL(file);
+    }
+
+    handleSubmit= e =>{
+    e.preventDefault();
+    let activeP = this.state.active === 'edit' ? 'profile' : 'edit';
+    this.setState({
+        active: activeP,
+    })
+    }
+    
+    render() {
+    const {imagePreviewUrl, 
+            name, 
+            status, 
+            active} = this.state;
+    return (
+        <div>
+        {(active === 'edit')?(
+            <Edit onSubmit={this.handleSubmit}>
+            <ImgUpload onChange={this.photoUpload} src={imagePreviewUrl}/>
+            </Edit>
+        ):(
+            <Profile 
+            onSubmit={this.handleSubmit} 
+            src={imagePreviewUrl} 
+            name={name} 
+            status={status}/>)}
+        
+        </div>
+    )
+    }
+}
+
+export default ProfilePic;
