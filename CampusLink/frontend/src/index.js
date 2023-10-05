0213b51db2c7c9b0dabd5bef1826626574922866
Phillip bernwanger <Phillip>
2023-10-05 17:23:12 -0400

    Removed App.js and added browserrouter to index

--- CampusLink/frontend/src/index.js
+++ CampusLink/frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css'
-import App from './App';
+import FriendPage from './pages/FriendPage'
 
 const appDiv = ReactDOM.createRoot(document.getElementById('app'));
 appDiv.render(
   <React.StrictMode>
-      <App />
+      <BrowserRouter>
+        <FriendPage/>
+      </BrowserRouter>
   </React.StrictMode>
 );
---
+++ CampusLink/frontend/src/pages/FriendPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Footer from "../components/Footer"
+
+export default function HomePage() {
+  return (
+    <div className="page-wrapper">
+        <div>FriendPage</div>
+        <Footer></Footer>
+    </div>
+  )
+}
