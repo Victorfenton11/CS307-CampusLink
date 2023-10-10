b52abc7301025c94b3048e1be81f4d9d3348fcdd
Michael Law <himimi15@gmail.com>
2023-10-09 23:03:45 -0400

    Added NavBar and created Page folder

---
+++ CampusLink/frontend/src/pages/CourseScheduler.js
@@ -0,0 +1,7 @@
+// page on course scheduler
+// import footer
+import Footer from "../components/Footer";
+// import logo
+import Logo from "../components/Logo";
+// import navbar
+import Navbar from "../components/Navbar";
