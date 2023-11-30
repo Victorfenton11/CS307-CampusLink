3aed5214db47153a0d4ce7b221eb9cbd0a34bda7
victorftn <vfenton@purdue.edu>
2023-11-30 18:02:50 -0500

    fixing profile style, add datetime picker

--- CampusLink/frontend/src/pages/Calendar.js
+++ CampusLink/frontend/src/pages/Calendar.js
@@ -6,6 +6,9 @@ import './styles/Calendar.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { useScrollTrigger } from '@material-ui/core';
 import Modal from '../components/Modal'
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 
 const localizer = momentLocalizer(moment);
 
@@ -39,18 +42,22 @@ export default function Calendar(props) {
     ]
 
   return (
-    <div className='calendar-page'>
-        <BigCalendar localizer={localizer} events={myEventsList} startAccessor="start" endAccessor="end" style={{ height: "90%" }} />
-        <div className='button-container'>
-            <Link to='/profile'><button className='calendar-button'>Back to Profile</button></Link>
-            <button className='calendar-button' onClick={handleAddClick}>Add Event</button>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <div className='calendar-page'>
+            <BigCalendar localizer={localizer} events={myEventsList} startAccessor="start" endAccessor="end" style={{ height: "90%" }} />
+            <div className='button-container'>
+                <Link to='/profile'><button className='calendar-button'>Back to Profile</button></Link>
+                <button className='calendar-button' onClick={handleAddClick}>Add Event</button>
+            </div>
+            <Modal show={showModal} handleClose={handleAddClick}>
+                <h4 className='title'>New Event</h4>
+                <input id="newEventName" className="inputBox" type="text" placeholder='Title'></input>
+                <input id="newEventDescription" className="inputBox" type="text" placeholder='Location'></input>
+                <DateTimePicker label="Starts" className='datetime-picker'/>
+                <DateTimePicker label="Ends" className='datetime-picker'/>
+                <button className='create-button' onClick={addEvent}>Create Event</button>
+            </Modal>
         </div>
-        <Modal show={showModal} handleClose={handleAddClick}>
-            <h4 className='title'>New Event</h4>
-            <input id="newEventName" className="inputBox" type="text" placeholder='Title'></input>
-            <input id="newEventDescription" className="inputBox" type="text" placeholder='Location'></input>
-            <button className='create-button' onClick={addEvent}>Create Event</button>
-        </Modal>
-    </div>
+    </LocalizationProvider>
   )
 }
