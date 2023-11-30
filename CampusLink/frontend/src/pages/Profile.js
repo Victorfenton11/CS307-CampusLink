8aaaed76e59e77dc29fb252000fedfb25ea89a6c
Michael Law <himimi15@gmail.com>
2023-11-30 15:20:40 -0500

    Anonymous Database Fixed
    Retrieval from useEffect added

--- CampusLink/frontend/src/pages/Profile.js
+++ CampusLink/frontend/src/pages/Profile.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import './styles/Profile.css'
-import dum_pic from '../../static/images/Test.jpg'
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./styles/Profile.css";
+import dum_pic from "../../static/images/Test.jpg";
+import { Link, useNavigate } from "react-router-dom";
 import Switch from "react-switch";
 
 const Profile = () => {
@@ -12,19 +12,9 @@ const Profile = () => {
   const [isLoading, setIsLoading] = useState(true);
   // State to track errors
   const [error, setError] = useState(null);
-
   const [imgError, setImgError] = useState(false);
-
   const [isEditMode, setIsEditMode] = useState(false);
-
   const [isPrivate, setIsPrivate] = useState(false);
-
-  const [isPrivateUser, setIsPrivateUser] = useState(false);
-  const [isPrivateUserName, setIsPrivateName] = useState(false);
-  const [isPrivateUserEmail, setIsPrivateEmail] = useState(false);
-  const [isPrivateUserPhoneNumber, setIsPrivatePhoneNumber] = useState(false);
-  const [isPrivateUserMajor, setIsPrivateMajor] = useState(false);
-
 
   // STATE FOR HIDING USER PROFILE SECTIONS
   const [checkedUser, setCheckedUser] = useState(false);
@@ -37,17 +27,17 @@ const Profile = () => {
   const fetchUserData = async (userID) => {
     try {
       // Make API request
-      const userID = sessionStorage.getItem('userID');
+      const userID = sessionStorage.getItem("userID");
       const response = await fetch(`/api/user/${userID}`);
-      
+
       // Check if the request was successful
       if (!response.ok) {
-        throw new Error('Failed to fetch user data');
+        throw new Error("Failed to fetch user data");
       }
 
       // Parse JSON response
       const data = await response.json();
-      
+
       // Set user data in state
       setUserData(data);
       // Set loading state to false
@@ -62,70 +52,68 @@ const Profile = () => {
   const handleEditClick = () => {
     setIsEditMode(true);
   };
-  const handleImgError= () => {
+  const handleImgError = () => {
     setImgError(true);
   };
   const onClickHandler = () => {
     imageGalleryRef.current.fullscreen();
   };
 
-  const imageUpload=(e)=>{
+  const imageUpload = (e) => {
     e.preventDefault();
 
-    const formData=new FormData();
-    formData.append("file",e.target.files[0],e.target.files[0].name);
+    const formData = new FormData();
+    formData.append("file", e.target.files[0], e.target.files[0].name);
 
-    fetch('/api/user/savefile',{
-        method:'POST',
-        body:formData
+    fetch("/api/user/savefile", {
+      method: "POST",
+      body: formData,
     });
-    setUserData({ ...userData, PhotoFileName: e.target.files[0].name});
+    setUserData({ ...userData, PhotoFileName: e.target.files[0].name });
   };
 
   // Function to handle the "Save" button click
-  const handleSaveClick = async (userID) => {
+  const handleSaveClick = async () => {
     // Perform logic to save the modified data to the API
-    try{
-      const userID = sessionStorage.getItem('userID');
+    try {
+      const userID = sessionStorage.getItem("userID");
       const response = await fetch(`/api/user/${userID}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(userData),
-    });
-    if (!response.ok) {
-      throw new Error('Failed to save user data');
-    }
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to save user data");
+      }
 
-    // Notify the user that the data was saved successfully
-    alert('Successfully changed the profile');
+      // Notify the user that the data was saved successfully
+      alert("Successfully changed the profile");
 
-    // Exit edit mode
-    setIsEditMode(false);
-    setImgError(false);
+      // Exit edit mode
+      setIsEditMode(false);
+      setImgError(false);
     } catch (error) {
-      console.error('Error saving user data:', error.message);
+      console.error("Error saving user data:", error.message);
     }
   };
-
-  function handlePrivate() {
-    setIsPrivate(true);
-    setUserData({ ...userData, isPrivate: true});
-  }
-  function handlePublic() {
-    setIsPrivate(false);
-    setUserData({ ...userData, isPrivate: false});
-  }
-
-  function handleCalendarClick() {
-    return navigate('/calendar');
-  }
 
   // UseEffect hook to fetch data when the component mounts
   useEffect(() => {
     fetchUserData();
   }, []);
+
+  useEffect(() => {
+    if (!userData) {
+      return;
+    }
+    setCheckedUser(userData.UserAnonymous);
+    setCheckedUserName(userData.UserNameAnonymous);
+    setCheckedUserEmail(userData.UserEmailAnonymous);
+    setCheckedUserPhoneNumber(userData.UserPhoneNumberAnonymous);
+    setCheckedUserMajor(userData.UserMajorAnonymous);
+  }, [userData]);
 
   // Render loading state
   if (isLoading) {
@@ -137,53 +125,74 @@ const Profile = () => {
     return <div>Error: {error}</div>;
   }
 
+  function handlePrivate() {
+    setIsPrivate(true);
+    setUserData({ ...userData, isPrivate: true });
+  }
+  function handlePublic() {
+    setIsPrivate(false);
+    setUserData({ ...userData, isPrivate: false });
+  }
+
+  function handleCalendarClick() {
+    return navigate("/calendar");
+  }
+
   // FUNCTIONS FOR HIDING USER PROFILE SECTIONS
   const handleChangeUser = (checkedUser) => {
     setCheckedUser(checkedUser);
     userData.UserAnonymous = !userData.UserAnonymous;
-  }
+  };
 
   const handleChangeUserName = (checkedUserName) => {
     setCheckedUserName(checkedUserName);
     userData.UserNameAnonymous = !userData.UserNameAnonymous;
-  }
+  };
 
   const handleChangeUserEmail = (checkedUserEmail) => {
     setCheckedUserEmail(checkedUserEmail);
     userData.UserEmailAnonymous = !userData.UserEmailAnonymous;
-  }
+  };
 
   const handleChangeUserPhoneNumber = (checkedUserPhoneNumber) => {
     setCheckedUserPhoneNumber(checkedUserPhoneNumber);
     userData.UserPhoneNumberAnonymous = !userData.UserPhoneNumberAnonymous;
-  }
+  };
 
   const handleChangeUserMajor = (checkedUserMajor) => {
     setCheckedUserMajor(checkedUserMajor);
     userData.UserMajorAnonymous = !userData.UserMajorAnonymous;
-  }
+  };
 
   if (isEditMode) {
     return (
       <div className="profile-style">
-        <div className='top'>Edit User Profile</div>
+        <div className="top">Edit User Profile</div>
         <label htmlFor="photo-upload" className="custom-file-upload fas">
-        <div className="img-wrap img-upload" >
-            <img htmlFor="photo-upload" src={imgError ?dum_pic:'../../static/images/' + userData.PhotoFileName} onError={handleImgError}/>
-        </div>
-        <input id="photo-upload" type="file" onChange={imageUpload}/> 
+          <div className="img-wrap img-upload">
+            <img
+              htmlFor="photo-upload"
+              src={
+                imgError
+                  ? dum_pic
+                  : "../../static/images/" + userData.PhotoFileName
+              }
+              onError={handleImgError}
+            />
+          </div>
+          <input id="photo-upload" type="file" onChange={imageUpload} />
         </label>
-        
+
         {/* NAME */}
-        <label className='label'>
+        <label className="label">
           Name:
           <input
             type="text"
-            value = {userData?.UserAnonymous && "Anonymous"
-              ? "Anonymous"
-              : `${userData.UserName}`
+            value={
+              userData?.UserAnonymous && "Anonymous"
+                ? "Anonymous"
+                : `${userData.Name}`
             }
-          
             onChange={(e) => setUserData({ ...userData, Name: e.target.value })}
           />
           <Switch
@@ -194,15 +203,18 @@ const Profile = () => {
           />
         </label>
         {/* USERNAME */}
-        <label className='label'>
+        <label className="label">
           UserName:
           <input
             type="text"
-            value = {userData?.UserNameAnonymous && "Anonymous"
-              ? "Anonymous"
-              : `${userData.UserName}`
+            value={
+              userData?.UserNameAnonymous && "Anonymous"
+                ? "Anonymous"
+                : `${userData.UserName}`
             }
-            onChange={(e) => setUserData({ ...userData, UserName: e.target.value })}
+            onChange={(e) =>
+              setUserData({ ...userData, UserName: e.target.value })
+            }
           />
           <Switch
             onChange={handleChangeUserName}
@@ -213,15 +225,18 @@ const Profile = () => {
         </label>
 
         {/* EMAIL */}
-        <label className='label'>
+        <label className="label">
           Email:
           <input
             type="text"
-            value = {userData?.UserEmailAnonymous && "Anonymous"
-              ? "Anonymous"
-              : `${userData.UserEmail}`
+            value={
+              userData?.UserEmailAnonymous && "Anonymous"
+                ? "Anonymous"
+                : `${userData.UserEmail}`
             }
-            onChange={(e) => setUserData({ ...userData, UserEmail: e.target.value })}
+            onChange={(e) =>
+              setUserData({ ...userData, UserEmail: e.target.value })
+            }
           />
           <Switch
             onChange={handleChangeUserEmail}
@@ -232,15 +247,18 @@ const Profile = () => {
         </label>
 
         {/* PHONE NUMBER */}
-        <label className='label'>
+        <label className="label">
           Phone Number:
           <input
             type="text"
-            value = {userData?.UserPhoneNumberAnonymous && "Anonymous"
-              ? "Anonymous"
-              : `${userData.UserPhoneNumber}`
+            value={
+              userData?.UserPhoneNumberAnonymous && "Anonymous"
+                ? "Anonymous"
+                : `${userData.UserPhoneNumber}`
             }
-            onChange={(e) => setUserData({ ...userData, PhoneNumber: e.target.value })}
+            onChange={(e) =>
+              setUserData({ ...userData, PhoneNumber: e.target.value })
+            }
           />
           <Switch
             onChange={handleChangeUserPhoneNumber}
@@ -251,15 +269,18 @@ const Profile = () => {
         </label>
 
         {/* MAJOR */}
-        <label className='label'>
+        <label className="label">
           Major:
           <input
             type="text"
-            value = {userData?.UserMajorAnonymous && "Anonymous"
-              ? "Anonymous"
-              : `${userData.UserMajor}`
+            value={
+              userData?.UserMajorAnonymous && "Anonymous"
+                ? "Anonymous"
+                : `${userData.UserMajor}`
             }
-            onChange={(e) => setUserData({ ...userData, Major: e.target.value })}
+            onChange={(e) =>
+              setUserData({ ...userData, Major: e.target.value })
+            }
           />
           <Switch
             onChange={handleChangeUserMajor}
@@ -269,55 +290,85 @@ const Profile = () => {
           />
         </label>
 
-
-
-        <button className='margin-topp' onClick={handlePrivate}>I want my information to be private</button>
-        <button onClick={handlePublic}>I want my information to be public</button>
-        <button className='save-btn' onClick={handleSaveClick}>Save</button>
+        <button className="margin-topp" onClick={handlePrivate}>
+          I want my information to be private
+        </button>
+        <button onClick={handlePublic}>
+          I want my information to be public
+        </button>
+        <button className="save-btn" onClick={handleSaveClick}>
+          Save
+        </button>
       </div>
     );
   }
 
   if (userData.isPrivate) {
     return (
-      <div className='profile-style'>
-      <div className='top'>User Profile</div>
-      <label className="custom-file-upload fas">
-        <div className="img-wrap" >
-            <img htmlFor="photo-upload" src={'../../static/images/' + userData.PhotoFileName} onClick={onClickHandler}/>
-        </div>
+      <div className="profile-style">
+        <div className="top">User Profile</div>
+        <label className="custom-file-upload fas">
+          <div className="img-wrap">
+            <img
+              htmlFor="photo-upload"
+              src={"../../static/images/" + userData.PhotoFileName}
+              onClick={onClickHandler}
+            />
+          </div>
         </label>
-        <div className='name'><label className='label'>Name:</label> {userData.Name}</div>
-        <div className='name'> <label className='label'>
-          UserName: </label>{userData.UserName}</div>
-          <button onClick={handleEditClick}>Edit</button>
-          <button onClick={handleCalendarClick}>My Calendar</button>
-    </div>
-    )
+        <div className="name">
+          <label className="label">Name:</label> {userData.Name}
+        </div>
+        <div className="name">
+          {" "}
+          <label className="label">UserName: </label>
+          {userData.UserName}
+        </div>
+        <button onClick={handleEditClick}>Edit</button>
+        <button onClick={handleCalendarClick}>My Calendar</button>
+      </div>
+    );
   }
 
   // Render user profile
   return (
-    <div className='profile-style'>
-      <div className='top'>User Profile</div>
+    <div className="profile-style">
+      <div className="top">User Profile</div>
       <label className="custom-file-upload fas">
-        <div className="img-wrap" >
-            <img htmlFor="photo-upload" src={'../../static/images/' + userData.PhotoFileName} onClick={onClickHandler}/>
+        <div className="img-wrap">
+          <img
+            htmlFor="photo-upload"
+            src={"../../static/images/" + userData.PhotoFileName}
+            onClick={onClickHandler}
+          />
         </div>
-        </label>
-        <div className='name'><label className='label'>Name:</label> {userData.Name}</div>
-        <div className='name'> <label className='label'>
-          UserName: </label>{userData.UserName}</div>
-        <div className='name'><label className='label'>
-          Email:</label>{userData.UserEmail}</div>
-        <div className='name'><label className='label'>
-          Phone Number:</label>{userData.PhoneNumber}</div>
-        <div className='name'><label className='label'>
-          Major:</label>{userData.Major}</div>
-          <div className='name'><label className='label'>
-          Interest:</label>{userData.Interest}</div>
-        <button onClick={handleEditClick}>Edit</button>
-        <button onClick={handleCalendarClick}>My Calendar</button>
+      </label>
+      <div className="name">
+        <label className="label">Name:</label> {userData.Name}
+      </div>
+      <div className="name">
+        {" "}
+        <label className="label">UserName: </label>
+        {userData.UserName}
+      </div>
+      <div className="name">
+        <label className="label">Email:</label>
+        {userData.UserEmail}
+      </div>
+      <div className="name">
+        <label className="label">Phone Number:</label>
+        {userData.PhoneNumber}
+      </div>
+      <div className="name">
+        <label className="label">Major:</label>
+        {userData.Major}
+      </div>
+      <div className="name">
+        <label className="label">Interest:</label>
+        {userData.Interest}
+      </div>
+      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleCalendarClick}>My Calendar</button>
     </div>
   );
 };
